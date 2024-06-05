5b2b75daa2d4a66ed2de45a5fea73014e8aebddc
Nathoggles <mawricus@gmail.com>
2024-06-05 13:33:17 +0200

    fleshing out basic variables

--- rps.js
+++ rps.js
@@ -1,1 +1,15 @@
 console.log('Testy!');
+let humanScore = 0;
+let computerScore = 0;
+//function getComputerChoice()
+function getComputerChoice(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+  }
+  console.log(getComputerChoice());
+
+let UserInput = prompt('Let\'s play Rock-Paper-Scissors! What do you pick? Type in your choice!', 'Rock')
+console.log(UserInput);
+
+  //function getHumanChoice(){}
