73c489d63b612cf6a013058d8bd9afc2a2606462
Nathoggles <mawricus@gmail.com>
2024-06-24 23:02:40 +0200

    correct rps.js

--- rps.js
+++ rps.js
@@ -43,9 +43,12 @@ function getHumanChoice(){
   //Rock is 1, Paper is 2, Scissors is 3. Which means (if excluding draws from the start), 
   //result 3 = Paper won over Rock, result 4 = Rock won over Scissors, result 5 = Scissors won over Rock.  
  
- /*function playGame(){
+
+
+
+  function playGame(){
   for (let i = 0; i <= 4; i++) {
-    playRound();*/
+    playRound();
 
 
 
@@ -100,8 +103,10 @@ if (computerScore < humanScore) {
 
 }
 
+//playGame();
 
-playGame();
+
+
 //TEMPLATES AND WIP CODE//
 /*So the main idea is to describe results as attributres and then do something like if result = 5 && player1input = 1 alert(win) else lost and give counter to comp/human.*/
 
