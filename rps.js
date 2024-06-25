a890df82ba29a3586a521ff3bdf2e10fb1b6ffb8
Nathoggles <mawricus@gmail.com>
2024-06-25 21:01:17 +0200

    adapt old code to promptless inputs

--- rps.js
+++ rps.js
@@ -1,5 +1,6 @@
 let humanScore = 0;
 let computerScore = 0;
+let i = 0;
   /*const pRock = document.querySelector("#rock");
   const pPaper = document.querySelector("#paper");
   const pScissors = document.querySelector("#scissors");
@@ -18,26 +19,49 @@ let computerScore = 0;
     return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
   }
 
+//Rock is 1, Paper is 2, Scissors is 3. Which means (if excluding draws from the start), 
+  //result 3 = Paper won over Rock, result 4 = Rock won over Scissors, result 5 = Scissors won over Rock.  
 
- /* let humanChoice;
+ let humanSelection;
 function getHumanChoice(){
    click.forEach((button) => {
     button.addEventListener("click", () => {
         console.log(button.id)
-        alert("Let's play!");
         if (button.id === 'paper') {
-          humanChoice = 2;
-          console.log(humanChoice);
+          humanSelection = 2;
+          console.log(humanSelection);
         } else if (button.id === 'scissors') {
-          humanChoice = 3;
-          console.log(humanChoice);
+          humanSelection = 3;
+          console.log(humanSelection);
          } else if (button.id === 'rock') {
-          humanChoice = 1;
-          console.log(humanChoice);
+          humanSelection = 1;
+          console.log(humanSelection);
           }
+     let computerSelection = getComputerChoice();
+     let roundResult = humanSelection + computerSelection;
+     if (humanSelection === computerSelection) {
+      alert(`Draw! You both chose ${button.id}. ${4 - i} rounds to go.`);
+    } else if (roundResult === 3 && humanSelection === 2) {
+      alert(`You won the round! You chose Paper, your opponent chose Rock. ${4 - i} rounds to go.`);
+      humanScore++;
+    } else if (roundResult === 4 && humanSelection === 1) {
+      alert(`You won the round! You chose Rock, your opponent chose Scissors. ${4 - i} rounds to go.`);
+      humanScore++;
+    } else if (roundResult === 5 && humanSelection === 3) {
+      alert(`You won the round! You chose Scissors, your opponent chose Rock. ${4 - i} rounds to go.`);
+      humanScore++;
+    } else if (typeof humanSelection === 'string') {
+      return
+    } else {
+    alert(`Sadly, you lost the round. Your opponent chose ${button.id}. Cheer up!' ${4 - i} rounds to go.`);
+    computerScore++; 
+    }
     });
   });
-};*/
+  
+};
+
+getHumanChoice();
 
 /*click.forEach((button) => {
   button.addEventListener("click", () => {
@@ -53,8 +77,7 @@ function getHumanChoice(){
 
  
 
-  //Rock is 1, Paper is 2, Scissors is 3. Which means (if excluding draws from the start), 
-  //result 3 = Paper won over Rock, result 4 = Rock won over Scissors, result 5 = Scissors won over Rock.  
+  
  
 
 
